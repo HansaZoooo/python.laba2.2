128c501d3d2497fd817fd91029554a52d8b40118
Heitota Vitalyy <fustvitalik0007@gmail.com>
2025-12-07 01:51:28 +0200

    commit

--- app_blog/urls.py
+++ app_blog/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import path
-from app_blog import views
+from app_blog.views import home   
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='home'),
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),  
 ]
--- app_blog/views.py
+++ app_blog/views.py
@@ -1,5 +1,4 @@
-from django.views.generic import TemplateView
 from django.shortcuts import render
 
-class HomePageView(TemplateView):
-    template_name = "index.html"
+def home(request):
+    return render(request, 'index.html')
